727ec02bec21cd6bd6d685bbe02f06178dd82508
meamruri <meammf@yandex.ru>
2018-03-10 17:19:28 +0300

    singleton demo was written

--- patterns-test/patterns_test.swift
+++ patterns-test/patterns_test.swift
@@ -10,18 +10,23 @@ import XCTest
 
 class patterns_test: XCTestCase {
     
+    var manager: NetworkManager!
+    
     override func setUp() {
         super.setUp()
+        manager = NetworkManager.shared()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        manager = nil
         super.tearDown()
     }
     
     func testSingleton() {
-        let isSimilarInstances = NetworkManager.shared() === NetworkManager.shared()
+        let otherInstance = NetworkManager.shared()
+        let isSimilarInstances = manager === otherInstance
         XCTAssertTrue(isSimilarInstances)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
--- patterns/Singleton/SingletonDemo.swift
+++ patterns/Singleton/SingletonDemo.swift
@@ -1,5 +1,5 @@
 //
-//  main.swift
+//  SingletonDemo.swift
 //  patterns
 //
 //  Created by Rom Dr on 10.03.2018.
@@ -8,4 +8,7 @@
 
 import Foundation
 
-singleton_main()
+func singleton_main() {
+    let m = NetworkManager.shared()
+    print(m.baseURL)
+}
